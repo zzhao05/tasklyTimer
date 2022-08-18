fa72e908bcc5e3e5cae16d78d375116a6fb5f555
zzhao05 <110996555+zzhao05@users.noreply.github.com>
2022-08-18 15:15:46 -0400

    Add files via upload
    updated timer

--- taskly/tasklyNav/tasklyNav/TimerViewController.swift
+++ taskly/tasklyNav/tasklyNav/TimerViewController.swift
@@ -6,16 +6,41 @@
 //
 
 import UIKit
+import UserNotifications
 
 
 class TimerViewController: UIViewController {
 
-   
+    
+    @IBOutlet weak var timeTextField: UITextField!
+    
     @IBOutlet weak var timerLabel: UILabel!
     
-    var time = 10
+    var time = 0
     
     var timer = Timer ()
+    
+    
+    
+    @IBAction func setButton(_ sender: UIButton) {
+        if let minutes = timeTextField.text{
+            
+            timerLabel.text = "\(minutes):00"
+            
+            
+            //time = timeTextField
+            time = (Int(minutes) ?? 0 )*60
+//        if let setTime = timeTextField.text{
+//            var numSetTime = (Int (setTime) ?? 0)*60
+//            timerLabel.text = numSetTime.text
+            
+            
+            
+        }
+    }
+    
+    
+    
     
     @IBAction func startButton(_ sender: UIButton) {
         timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(TimerViewController.action), userInfo: nil, repeats: true)
@@ -30,7 +55,7 @@ class TimerViewController: UIViewController {
     @IBAction func resetButton(_ sender: UIButton) {
         timer.invalidate()
         time = 0
-        timerLabel.text = "0"
+        timerLabel.text = "0:00"
     }
     
     @objc func action(){
@@ -39,8 +64,36 @@ class TimerViewController: UIViewController {
         }
         else if time == 0 {
             time = 0
+            showPopUp()
+            timer.invalidate()
+            
         }
-        timerLabel.text = String(time)
+        
+        // Make time look pretty
+        var minutes = time / 60
+        var seconds = time % 60
+        
+        if seconds < 10 {
+            timerLabel.text = "\(minutes):0\(seconds)"
+        }
+        else{
+            timerLabel.text = "\(minutes):\(seconds)"
+        }
+        
+       
+    }
+    
+    func showPopUp() {
+        let alert = UIAlertController(title: "Timer Done!", message: "YAY!", preferredStyle: .alert)
+        
+        let okayAction = UIAlertAction(title: "Ok", style: .default){
+            (action)in
+            print(action)
+        }
+        
+        alert.addAction(okayAction)
+        
+        present(alert, animated: true, completion: nil)
     }
     
     
